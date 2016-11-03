6f973bba0b2f4ce876e4c1f854f17e94844b7378
Giurito <giulio.cruz.o@gmail.com>
2016-11-02 22:41:47 -0200

    Update Filtro_horario.sql

--- Filtro_horario.sql
+++ Filtro_horario.sql
@@ -4,13 +4,13 @@
 #  on horario.disciplina = disciplina.id
 #  WHERE horario.inicio = '7:30' and termino = '9:20';
   
-drop view if exists disciplinas_horario;
+drop view if exists filtro_disciplinas_horario;
 
-CREATE VIEW disciplinas_horario AS
+CREATE VIEW filtro_disciplinas_horario AS
 SELECT horario.disciplina
   FROM horario
   inner join disciplina
   on horario.disciplina = disciplina.id
   WHERE horario.inicio = '7:30' and termino = '9:20';
 
-select * from disciplinas_horario;
+select * from filtro_disciplinas_horario;
