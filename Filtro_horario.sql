af336b55f823ad462671d7d614470ea889aa1d2c
Giurito <giulio.cruz.o@gmail.com>
2016-11-04 06:29:48 -0200

    Update Filtro_horario.sql

--- Filtro_horario.sql
+++ Filtro_horario.sql
@@ -14,3 +14,19 @@ SELECT horario.disciplina
   WHERE horario.inicio = '7:30' and termino = '9:20';
 
 select * from filtro_disciplinas_horario;
+
+
+------------------------------------------
+
+DELIMITER $$
+CREATE PROCEDURE get_disciplina_horario(init time,term time)
+BEGIN
+SELECT horario.disciplina
+  FROM horario
+  inner join disciplina
+  on horario.disciplina = disciplina.id
+  WHERE horario.inicio = init and termino = term;
+END$$
+DELIMITER ;
+
+CALL get_disciplina_horario('7:30','9:20');
